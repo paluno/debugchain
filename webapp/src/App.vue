e369975bda4e2777f0b91c025007e05733d4fa84
Philipp Disselhoff <philipp.disselhoff@stud.uni-due.de>
2018-06-05 23:35:03 +0200

    Extract Navigation to component.

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,30 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Issue Overview</router-link> |
-      <router-link to="/profile">Profile</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navigation/>
     <router-view/>
   </div>
 </template>
 
+<script>
+import Navigation from "@/components/Navigation.vue";
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
