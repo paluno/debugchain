c7ae48d109a9514b12818b7c8251e136c3b4d6ee
Fabian <fabian.glittenberg@stud.uni-duisburg-essen.de>
2018-06-10 13:41:15 +0200

    Added ProjectSetup component

---
+++ webapp/src/components/ProjectSetup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="projectsetup">
+    <form @submit.prevent="createProject">
+        <input type="text" placeholder="Enter the GitLab-ID..." v-model="id">
+        <button @click="createProject" type="button">Create Project</button>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectSetup",
+  data() {
+    return {
+        id: "",
+    }
+  },
+  methods: {
+      createProject() {
+          //TODO meta mask + backend calls
+          console.log(this.id);
+      }
+  }
+};
+</script>
