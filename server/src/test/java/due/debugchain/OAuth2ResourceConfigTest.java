9f8c833ae1d9715f575daa74da153df9108e0cbf
Sebastian Adams <sebastian.adams@me.com>
2018-06-14 19:46:31 +0200

    bug fixed oauth conf test

--- server/src/test/java/due/debugchain/OAuth2ResourceConfigTest.java
+++ server/src/test/java/due/debugchain/OAuth2ResourceConfigTest.java
@@ -1,22 +1,32 @@
 package due.debugchain;
 
+import due.debugchain.persistence.entities.ProjectEntity;
+import org.json.JSONObject;
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class OAuth2ResourceConfigTest extends IntegrationTest {
 
     @Test
-    public void denyApiWithoutToken() throws Exception {
-        mockMvc.perform(get("/blocks"))
-            .andExpect(status().isUnauthorized());
+    public void apiAuthTestWithToken() throws Exception {
+        mockMvc.perform(get("/api/projects")
+                .with(userToken())
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void allowApiWithToken() throws Exception {
-        mockMvc.perform(get("/blocks")
-            .with(userToken()))
-            .andExpect(status().isOk());
+    public void apiAuthTestWithoutToken() throws Exception {
+        mockMvc.perform(get("/api/projects")
+                .accept(APPLICATION_JSON)
+                .contentType(APPLICATION_JSON_UTF8))
+                .andExpect(status().isUnauthorized());
     }
 }
