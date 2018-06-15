e899157c68f1cd470c02650080033ecc4b1c6915
Fabian <fabian.glittenberg@stud.uni-duisburg-essen.de>
2018-06-15 15:09:24 +0200

    Removed pathRewrite to not remove /api of http calls to backend server anymore

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -3,8 +3,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api" : {
-        target: "http://localhost:8081",
-        pathRewrite: {"^/api" : ""}
+        target: "http://localhost:8081"
       }
     }
   },
