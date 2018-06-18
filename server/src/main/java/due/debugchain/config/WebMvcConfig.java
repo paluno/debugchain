9e3ae343f6231883dd97f41333c636e1f85158bf
Sebastian Adams <sebastian.adams@me.com>
2018-06-18 19:33:41 +0200

    redirect all request from "/" to "index.html"

--- server/src/main/java/due/debugchain/config/WebMvcConfig.java
+++ server/src/main/java/due/debugchain/config/WebMvcConfig.java
@@ -6,8 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -24,5 +29,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(
         List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(new ProjectResolver(projectService));
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index.html");
     }
 }
