1c8f84bb4230cec11a8d618cd2283844c69817bb
Lenard Gaida <lenard.gaida@googlemail.com>
2018-06-20 14:38:20 +0200

    add layout for issuedetail

--- webapp/src/views/IssueDetail.vue
+++ webapp/src/views/IssueDetail.vue
@@ -1,10 +1,38 @@
 <template>
   <div class="issue_detail">
     <Navigation v-bind:projectId="projectId"/>
-    <h1>This is an issue detail page.</h1>
+    <h1><a href="gitlabLink" title="Click to watch issue in Gitlab" class="badge badge-success">Open</a> IssueTitle</h1>
+    <div class="row">
+      <div class="col">
+        <label>Created</label>
+        <span>10 Minutes ago</span>
+        <label>by</label>
+        <span>User </span><img class="avatar" src="https://www.gravatar.com/avatar/e64c7d89f26bd1972efa854d13d7dd61?s=48&d=identicon"/>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col">
+        <p>
+          Hier soll die Beschreibung des Issues rein. Damit man auch lesen kann wofür das Issue steht
+        </p>
+      </div>
+    </div>
+    <hr>
+    <div class="form-group row">
+      <div class="col">
+        <button class="btn btn-outline-secondary btn-sm">Donate Ether</button>
+      </div>
+    </div>
   </div>
 </template>
-
+<style>
+.avatar {
+  border-radius: 50%;
+  height: 25px;
+  width: 25px;
+}
+</style>
 <script>
 import Navigation from "@/components/Navigation";
 
