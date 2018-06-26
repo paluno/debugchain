157c0063c3b0585a15706e0351c3fd005a345e6d
Philipp Disselhoff <philipp.disselhoff@stud.uni-due.de>
2018-06-26 17:14:51 +0200

    Perform login redirect without loading login page (before routing).

--- webapp/src/views/Login.vue
+++ webapp/src/views/Login.vue
@@ -29,37 +29,44 @@ export default {
       session: UserSession.state
     };
   },
-  created: function() {
-    if (this.session.loggedIn) {
-      // User is already authenticated. Perform redirect, if requested
-      this.performRedirect();
-    } else {
+  beforeRouteEnter: function(to, from, next) {
+    var redirect = null;
+    if (to.query.hasOwnProperty("redirect")) {
+      // get redirect from query
+      redirect = to.query.redirect;
+    }
+    if (!redirect) {
+      // get redirect from storage (redirect coming from gitlab)
+      redirect = Storage.getLoginRedirect();
+      Storage.setLoginRedirect(null);
+    }
+
+    if (window.location.href.includes("access_token")) {
       // if #access_token exists, set token, then redirect, if requested
-      const that = this;
-      if (window.location.href.includes("access_token")) {
-        GitlabOAuth.token.getToken(window.location.href).then(function(token) {
-          UserSession.login(token.accessToken);
-          that.performRedirect();
-        });
+      GitlabOAuth.token.getToken(window.location.href).then(token => {
+        UserSession.login(token.accessToken);
+        if (redirect) {
+          next(redirect);
+        } else {
+          next();
+        }
+      });
+    } else if (UserSession.state.loggedIn) {
+      // if already logged in, redirect, if requested
+      if (redirect) {
+        next(redirect);
+      } else {
+        next();
       }
+    } else {
+      // not logged in and no access_token > no action / show login
+      next();
     }
   },
   methods: {
     loginViaGitlab: function(event) {
       Storage.setLoginRedirect(this.$route.query.redirect);
       window.location.href = GitlabOAuth.token.getUri();
-    },
-    performRedirect: function() {
-      if (this.$route.query.hasOwnProperty("redirect")) {
-        this.$router.push(this.$router.query.redirect);
-        return;
-      }
-      const redirect = Storage.getLoginRedirect();
-      if (redirect != null) {
-        this.$router.push(redirect);
-
-        Storage.setLoginRedirect(null);
-      }
     }
   }
 };
