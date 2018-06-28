8413c92420ed777e24ea7a13f16432b54c77fbcc
Nils Mehlhorn <nils.mehlhorn@stud.uni-due.de>
2018-06-28 20:22:46 +0200

    Put correct private key for testing back in

--- server/src/test/java/due/debugchain/IntegrationTest.java
+++ server/src/test/java/due/debugchain/IntegrationTest.java
@@ -43,7 +43,7 @@ public abstract class IntegrationTest {
 
     // private key from ganache-cli using mnemonic:
     // 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
-    private static final String PRIVATE_KEY = "2631b689696e69aae84657d1d1f1bebae62bd50af72dfff2ef3050c9828bf8f3";
+    private static final String PRIVATE_KEY = "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
 
     @Autowired
     private RestTemplate restTemplate;
