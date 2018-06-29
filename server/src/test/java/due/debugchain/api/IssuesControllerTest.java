e53e67ac7dc6413db25a0554255a8035b4bd8560
Sebastian Adams <sebastian.adams@me.com>
2018-06-29 08:25:53 +0200

    implemented IssuesController tests

--- server/src/test/java/due/debugchain/api/IssuesControllerTest.java
+++ server/src/test/java/due/debugchain/api/IssuesControllerTest.java
@@ -2,14 +2,14 @@ package due.debugchain.api;
 
 import due.debugchain.IntegrationTest;
 import due.debugchain.contracts.DebugChain;
+import due.debugchain.persistence.ProjectService;
+import due.debugchain.persistence.entities.ProjectEntity;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static java.math.BigInteger.valueOf;
-import static org.assertj.core.api.Assertions.assertThat;
+import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.iterableWithSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -19,36 +19,43 @@ public class IssuesControllerTest extends IntegrationTest {
 
     private DebugChain contract;
 
+    @Autowired
+    private ProjectService projectService;
+
     @Before
     public void setup() throws Exception {
         contract = deployContract();
         contract.donate(valueOf(1L), valueOf(1L)).send();
         contract.donate(valueOf(2L), valueOf(1L)).send();
-    }
 
-
-    @Test // TODO remove
-    public void addIssues() throws Exception{
-        List issues = contract.getIssueLookup().send();
-        assertThat(issues.size()).isEqualTo(2);
+        String adress = contract.getContractAddress();
+        ProjectEntity project = new ProjectEntity();
+        project.setGitlabId(1L);
+        project.setAddress(adress);
+        projectService.addProject(project);
     }
 
     @Test
-    @Ignore // TODO implement
     public void getIssuesWithId() throws Exception {
-        mockMvc.perform(get("/api/projects/1/issues/1")
-                .with(userToken()))
-                .andExpect(status().isOk());
-
-    }
-
-    @Test
-    @Ignore // TODO implement
-    public void getIssueList() throws Exception {
-        mockMvc.perform(get("/api/projects/1/issues")
+        System.out.println(mockMvc.perform(get("/api/projects/1/issues/1")
                 .with(userToken()))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", iterableWithSize(2)));
+                .andExpect(jsonPath("$[?(@.id == 1 && @.donationSum == 1)]").exists())
+                .andReturn().getResponse().getContentAsString());
+
+    }
+
+    @Test
+    public void getIssueList() throws Exception {
+        System.out.println(mockMvc.perform(get("/api/projects/1/issues")
+                .with(userToken()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", iterableWithSize(2)))
+                .andExpect(jsonPath("$[0].id", is(1)).exists())
+                .andExpect(jsonPath("$[0].donationSum", is(1)).exists())
+                .andExpect(jsonPath("$[1].id", is(2)).exists())
+                .andExpect(jsonPath("$[1].donationSum", is(1)).exists())
+                .andReturn().getResponse().getContentAsString());
     }
 
 
