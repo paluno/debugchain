dfb182e021e777d86515456782d8c86bdfa9449b
Nils Mehlhorn <nils.mehlhorn@stud.uni-due.de>
2018-07-03 14:52:23 +0200

    Update contract instance variable name

--- webapp/src/api/contract.js
+++ webapp/src/api/contract.js
@@ -4,41 +4,34 @@ import byteCode from '../../contracts/___contracts_contracts_DebugChain_sol_Debu
 
 export default class Contract {
 
-    constructor(address, rpcUrl) {
-        if (rpcUrl) {
-            this.web3 = new Web3(new Web3.providers.HttpProvider(rpcUrl));
-        } else {
-            this.web3 = getWeb3();
-        }
-        this.contract = this.web3.eth.contract(abi);
+    constructor(address) {
+        this.web3 = getWeb3();
+        this.instance = this.web3.eth.contract(abi);
         if (address) {
-            this.contract = this.contract.at(address);
+            this.instance = this.instance.at(address);
         }
     }
 
     deploy(projectId) {
         return new Promise((resolve, reject) => {
-            const address = this.contract.address;
+            const address = this.instance.address;
             if (address) {
                 reject(new Error('Contract already deployed at ' + address));
             }
-            let firstCall = true;
-            this.contract.new(
+            this.instance.new(
                 projectId,
-                { data: byteCode, from: this.web3.eth.accounts[0], gas: 4712388 },
+                {data: byteCode, from: this.web3.eth.accounts[0]},
                 (err, contract) => {
                     if (err) {
                         reject(err);
                     }
                     // callback is called twice, address is only present the second time
-                    if (firstCall) {
-                        firstCall = false;
-                    } else {
-                        this.contract = contract;
+                    if (contract.address) {
+                        this.instance = contract;
                         resolve(contract.address);
                     }
                 }
             );
-        });
+        })
     }
 }
