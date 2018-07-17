7bb4e73e2f21fd8395ba87fd19ffa2f927ed192d
Fabian <fabian.glittenberg@stud.uni-duisburg-essen.de>
2018-07-17 17:45:33 +0200

    Add Navigation Bar to FAQ site

--- webapp/src/views/FAQ.vue
+++ webapp/src/views/FAQ.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
+    <div class="FAQ">
+        <Navigation />
         <h2> FAQ site </h2>
     </div>
 </template>
 
 <script>
+import Navigation from "@/components/Navigation";
+
 export default {
   name: "faq",
+  components: {
+      Navigation
+  },
   data() {
     return {
       
