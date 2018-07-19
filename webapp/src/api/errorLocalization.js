8b3af64bd4a076f299010d7f66812cb4862c0244
Philipp Disselhoff <philipp.disselhoff@stud.uni-due.de>
2018-07-19 22:16:02 +0200

    Display service name for gitlab and backend errors.

--- webapp/src/api/errorLocalization.js
+++ webapp/src/api/errorLocalization.js
@@ -1,19 +1,19 @@
 class ErrorLocalization {
 
-    getForAxios(error) {
+    getForAxios(error, serviceName) {
         if (error.response) {
             // The request was made and the server responded with an error status code
             switch (error.response.status) {
                 case 404:
-                    return `The requested resource does not exist. Try going back to the last page.`;
+                    return `The requested resource does not exist${serviceName ? " in " + serviceName : ""}. Try going back to the last page.`;
                 case 500:
-                    return `An internal error occured. We're sorry, please try again later.`;
+                    return `An internal error occured${serviceName ? " in " + serviceName : ""}. We're sorry, please try again later.`;
                 default:
-                    return `The server returned an error: ${error.response.status} ${error.response.statusText}`;
+                    return `The${serviceName ? " in " + serviceName : ""} server returned an error: ${error.response.status} ${error.response.statusText}`;
             }
         } else if (error.request) {
             // The request was made but no response was received
-            return `The server did not respond. Try checking your internet connection.`;
+            return `The${serviceName ? " in " + serviceName : ""} server did not respond. Try checking your internet connection.`;
         }
         return null;
     }
--- webapp/src/api/gitlab.js
+++ webapp/src/api/gitlab.js
@@ -31,7 +31,7 @@ export class Gitlab {
             // return data directly: we usually dont need detailed response information on success
             response => response.data,
             error => {
-                error.userMessage = Localization.getForAxios(error);
+                error.userMessage = Localization.getForAxios(error, "Gitlab");
                 return Promise.reject(error);
             });
     }
